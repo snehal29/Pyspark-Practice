8e78f49a62a6a1916cc4ac7b97c7f7b0a49c5e7e
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-24 19:05:51 +0530

    Update 04_Palindrom_or_not.py

--- Python_Coding_Practice/04_Palindrom_or_not.py
+++ Python_Coding_Practice/04_Palindrom_or_not.py
@@ -1,1 +1,11 @@
-
+'''
+Write a program to check the given string is palindrom or not
+'''
+def isPalindrome(phrase):
+  s1= phrase.lower().replace(' ','').replace('.','') # make string to lowercase and replace spaces and .,! 
+  if s1==s1[::-1]:          # do string scling and string compare
+      return True           # If palindrome then return true
+  else:
+      return False          # else palindrome then return False
+  
+print(isPalindrome('Taco cat.'))
